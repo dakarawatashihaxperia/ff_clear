3a7e7c850d52d426cc483159885ed6c9efa4fd5c
aiiso <aiiso>
2018-07-26 13:06:05 +0900

    add clear content

--- background.js
+++ background.js
@@ -5,4 +5,11 @@
  */
 browser.browserAction.onClicked.addListener(() => {
     browser.browsingData.removeCache({});
+    browser.browsingData.removeCookies({});
+    browser.browsingData.removeDownloads({});
+    browser.browsingData.removeFormData({});
+    browser.browsingData.removeHistory({});
+    browser.browsingData.removeLocalStorage({});
+    browser.browsingData.removePasswords({});
+    browser.browsingData.removePluginData({});
 });
